a6278950f78cb17a9d7920b9ed1fdf8dc1ec6ad7
Souradip5 <38311782+soura76developer@users.noreply.github.com>
2022-01-12 20:38:22 +0530

    Add files via upload

---
+++ comp.js
@@ -0,0 +1,22 @@
+import react,{component} from 'react';
+
+class Comp extends component
+{
+    render()
+    {
+        return
+        (
+        <>
+            
+        <h1>Hi </h1>    
+            
+            
+        </>
+
+
+        )
+
+    }
+}
+
+export default Comp;
---
+++ index.js
@@ -0,0 +1,5 @@
+import ReactDom from 'react-dom';
+import react from 'react';
+import comp from './comp';
+
+ReactDom.render(<comp />,document.getElementById("root"));
